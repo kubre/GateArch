91c72adb3d56f9b977019d031cec2bc8c39496b6
Vaibhav Kubre <v@kubre.in>
2020-07-05 09:15:41 +0530

    Fixed: Login for student when admin logged in

--- app/Http/Controllers/Student/RegisterController.php
+++ app/Http/Controllers/Student/RegisterController.php
@@ -12,7 +12,7 @@ class RegisterController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('guest:student');
     }
 
     public function showRegisterForm()
