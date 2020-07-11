96de4a0e7c79fd608119f2bd71bd157bc82eed5a
Vaibhav Kubre <v@kubre.in>
2020-07-12 00:26:31 +0530

    Added under construction message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,8 @@
 @extends('students.exam.master')
 
 @section('content')
-<div class="container mt-4">
-    <h3 class="text-center">Demo App by <a href="//kubre.in">kubre</a></h3>
-    <div class="d-flex justify-content-center mt-3">
-        <a href="/students/login" class="btn btn-success mr-2">Student Portal</a>
-        <a href="/admin" class="btn btn-success">Admin Panel</a>
-    </div>
+<div class="container mt-5 pt-5 d-flex flex-column justify-content-center align-items-center">
+    <i class="material-icons" style="font-size: 5rem">settings</i>
+    <h1 class="text-center display-3">Under Construction</h1>
 </div>    
 @endsection
