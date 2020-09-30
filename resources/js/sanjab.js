18ce560df147d57eec4cfea509846952dbd54a07
Vaibhav Kubre <v@kubre.in>
2020-09-30 20:06:01 +0530

    elfinder fixed

--- resources/js/sanjab.js
+++ resources/js/sanjab.js
@@ -4,8 +4,8 @@ window.tinymce = require('tinymce').default
 Vue.component('tiny-mce', require('./widgets/TinyMCE.vue').default);
 
 if (document.querySelector('#sanjab_app')) {
-    $('.sidebar').data('image', '');
-    $('.sidebar-background').css('background-image', '');
+    // $('.sidebar').data('image', '');
+    // $('.sidebar-background').css('background-image', '');
     $('.image-author').remove();
 
     window.sanjabApp = new Vue({
