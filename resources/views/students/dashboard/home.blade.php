019364e34edfbcd0eb5b2c493d70b29affd5a501
Vaibhav Kubre <v@kubre.in>
2021-11-02 12:39:35 +0530

    disabled email verification

--- resources/views/students/dashboard/home.blade.php
+++ resources/views/students/dashboard/home.blade.php
@@ -4,63 +4,69 @@
 @endsection
 
 @section('content')
-<div class="container-fluid px-5">
+    <div class="container-fluid px-5">
 
-    @if(date('dm') == date('dm', strtotime($user->dob)))
-    <div style="width: max-content" class="card mx-auto">
-        <div class="card-body">
-            🎂 &nbsp;&nbsp; Happy Birthday, {{ explode(" ", $user->name)[0] }}!
-        </div>
-    </div>
-    @endif
-    <div class="card mx-auto bg-gate shadow-md">
-        <div class="card-body">
-           <strong>Note: Please use desktop/laptop device to take exams.</strong>
-        </div>
-    </div>
-
-
-
-    @empty($user->email_verified_at)
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header card-header-warning">{{ __('Verify Your Email Address') }}</div>
+        @if (date('dm') == date('dm', strtotime($user->dob)))
+            <div style="width: max-content" class="card mx-auto">
                 <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                    🎂 &nbsp;&nbsp; Happy Birthday, {{ explode(' ', $user->name)[0] }}!
                 </div>
             </div>
-        </div>
-    </div>
-    @endempty
-
-    @if($post)
-    <h3>Latest Blog Post</h3>
-    <a style="display: block" href="{{ route('posts', [$post->id,$post->slug]) }}">
-    <div class="container-fluid d-flex flex-column justify-content-center pb-1 post-wrapper card">
-        <div class="card-body">
-        <h3 class="card-title text-gate">{{ $post->title }}</h3>
-            <div class="mt-2 row">
-                <div class="d-flex align-items-center col-md-6"><span class="material-icons mr-2">today</span>Published On: &nbsp;<strong>{{ $post->publish_date }}</strong></div>
-                <div class="d-flex align-items-center col-md-6"><span class="material-icons mr-2">person</span>Written By: &nbsp;<strong>{{ $post->writer_name }}</strong></div>
+        @endif
+        <div class="card mx-auto bg-gate shadow-md">
+            <div class="card-body">
+                <strong>Note: Please use desktop/laptop device to take exams.</strong>
             </div>
-        <div class="d-flex align-items-center my-2"><span class="material-icons mr-2">label</span>Tags: &nbsp;<strong>{{ $post->tags }}</strong></div>
         </div>
-    </div>
-    </a>
-    @endif
-    {{-- @empty ($user->member_at)
+
+
+
+        {{-- @empty($user->email_verified_at)
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-warning">{{ __('Verify Your Email Address') }}</div>
+                        <div class="card-body">
+                            @if (session('resent'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                </div>
+                            @endif
+
+                            {{ __('Before proceeding, please check your email for a verification link.') }}
+                            {{ __('If you did not receive the email') }},
+                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                                @csrf
+                                <button type="submit"
+                                    class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endempty --}}
+
+        @if ($post)
+            <h3>Latest Blog Post</h3>
+            <a style="display: block" href="{{ route('posts', [$post->id, $post->slug]) }}">
+                <div class="container-fluid d-flex flex-column justify-content-center pb-1 post-wrapper card">
+                    <div class="card-body">
+                        <h3 class="card-title text-gate">{{ $post->title }}</h3>
+                        <div class="mt-2 row">
+                            <div class="d-flex align-items-center col-md-6"><span
+                                    class="material-icons mr-2">today</span>Published On:
+                                &nbsp;<strong>{{ $post->publish_date }}</strong></div>
+                            <div class="d-flex align-items-center col-md-6"><span
+                                    class="material-icons mr-2">person</span>Written By:
+                                &nbsp;<strong>{{ $post->writer_name }}</strong></div>
+                        </div>
+                        <div class="d-flex align-items-center my-2"><span class="material-icons mr-2">label</span>Tags:
+                            &nbsp;<strong>{{ $post->tags }}</strong></div>
+                    </div>
+                </div>
+            </a>
+        @endif
+        {{-- @empty($user->member_at)
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -75,37 +81,40 @@
     </div>
     @endempty --}}
 
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-success card-header-icon">
-                            <div class="card-icon bg-gate">
-                            <i class="material-icons">content_paste</i>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-success card-header-icon">
+                                <div class="card-icon bg-gate">
+                                    <i class="material-icons">content_paste</i>
+                                </div>
+                                <p class="card-category">Total Exams Modules Available</p>
+                                <h3 class="card-title">{{ $exams_count }}</h3>
                             </div>
-                            <p class="card-category">Total Exams Modules Available</p>
-                            <h3 class="card-title">{{ $exams_count }}</h3>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <a href='{{ route('students.exams') }}' class='btn bg-gate-alt btn-info btn-sm text-white py-2 px-3'>More</a>
+                            <div class="card-footer">
+                                <div>
+                                    <a href='{{ route('students.exams') }}'
+                                        class='btn bg-gate-alt btn-info btn-sm text-white py-2 px-3'>More</a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card card-stats">
-                        <div class="card-header card-header-danger card-header-icon">
-                            <div class="card-icon bg-gate">
-                            <i class="material-icons">library_books</i>
+                    <div class="col-md-6">
+                        <div class="card card-stats">
+                            <div class="card-header card-header-danger card-header-icon">
+                                <div class="card-icon bg-gate">
+                                    <i class="material-icons">library_books</i>
+                                </div>
+                                <p class="card-category">Total Results</p>
+                                <h3 class="card-title">{{ $results_count }}</h3>
                             </div>
-                            <p class="card-category">Total Results</p>
-                            <h3 class="card-title">{{ $results_count }}</h3>
-                        </div>
-                        <div class="card-footer">
-                            <div>
-                                <a href='{{ route('students.results') }}' class='btn bg-gate-alt btn-info btn-sm text-white py-2 px-3'>More</a>
+                            <div class="card-footer">
+                                <div>
+                                    <a href='{{ route('students.results') }}'
+                                        class='btn bg-gate-alt btn-info btn-sm text-white py-2 px-3'>More</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -113,5 +122,4 @@
             </div>
         </div>
     </div>
-</div>
 @endsection
